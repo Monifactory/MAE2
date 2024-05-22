8c03743a80fbfcd66ca5d789455190eed67b687b
Stone <samstone9001@gmail.com>
2024-05-22 13:45:27 -0400

    idk trying to get it to work

---
+++ src/main/java/stone/mae2/Config.java
@@ -0,0 +1,34 @@
+package stone.mae2;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = MAE2.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    private static final ForgeConfigSpec.BooleanValue INTERFACE_P2P = BUILDER
+            .comment("Whether the Interface P2P is enabled")
+            .define("interfaceP2P", true);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        ConfigProxy.INTERFACE_P2P = INTERFACE_P2P.get();
+    }
+}
---
+++ src/main/java/stone/mae2/ConfigProxy.java
@@ -0,0 +1,7 @@
+package stone.mae2;
+
+public class ConfigProxy {
+
+	public static Boolean INTERFACE_P2P;
+
+}
