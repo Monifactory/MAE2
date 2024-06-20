239c41194d10170a42b3429db2582c26e4ab3ccb
stone <samstone9001@gmail.com>
2024-06-19 22:50:00 -0400

    Stock Multi-input P2Ps added

--- src/main/java/stone/mae2/mixins/P2PTunnelPartMixin.java
+++ src/main/java/stone/mae2/mixins/P2PTunnelPartMixin.java
@@ -1,11 +1,6 @@
 package stone.mae2.mixins;
 
-import appeng.api.implementations.items.IMemoryCard;
-import appeng.api.networking.IGrid;
-import appeng.api.networking.IManagedGridNode;
-import appeng.api.parts.IPart;
 import appeng.api.parts.IPartItem;
-import appeng.me.service.P2PService;
 import appeng.parts.AEBasePart;
 import appeng.parts.p2p.P2PTunnelPart;
 import appeng.util.SettingsFrom;
@@ -13,21 +8,15 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.Vec3;
-import stone.mae2.MAE2;
 import stone.mae2.api.features.MultiP2PTunnelAttunement;
 
-import java.util.Objects;
-
-import com.llamalad7.mixinextras.sugar.Local;
-
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = P2PTunnelPart.class, remap = false)
@@ -43,44 +32,21 @@ public abstract class P2PTunnelPartMixin extends AEBasePart {
         return player.getMainHandItem().isEmpty() ? player.getOffhandItem() : player.getMainHandItem();
     }
 
-    @Inject(method = "onPartActivate", at = @At(ordinal = 0, value = "INVOKE", target = "importSettings", shift = At.Shift.BEFORE), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-    public void onUpdateFreq(Player player, InteractionHand hand, Vec3 pos, CallbackInfoReturnable<Boolean> ci, ItemStack is, IMemoryCard mc, CompoundTag configData, IPartItem<P2PTunnelPart<?>> item, IPart part ,P2PTunnelPart<?> newTunnel) {
-        System.out.println("onImportSettings");
-        boolean isInput = player.getMainHandItem().isEmpty()
-                && player.getOffhandItem().getItem() instanceof IMemoryCard;
-        if (!isInput)
-            return;
-        System.out.println("Cancelling importSettings");
-        IGrid grid = getMainNode().getGrid();
-        short freq = configData.getShort(CONFIG_NBT_FREQ);
-        System.out.println(configData);
-        if (grid == null) {
-            System.out.println("Null grid");
-            newTunnel.setFrequency(freq);
-            ci.setReturnValue(true);
-            return;
+    @Inject(method = "importSettings", at = @At(ordinal = 0, value = "INVOKE", target = "setOutput", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void afterSetOutput(SettingsFrom mode, CompoundTag input, @Nullable Player player, CallbackInfo ci) {
+        if (mode == SettingsFrom.MEMORY_CARD) {
+            if (player != null) {
+                if (player.getMainHandItem().isEmpty()) {
+                    setOutput(false);
+                    return;
+                }
+            }
         }
-        P2PService service = P2PService.get(grid);
-        P2PTunnelPart<?> input = service.getInput(freq);
-        System.out.println("Input: "+ Objects.toString(input));
-        System.out.println("NewTunnel: "+ newTunnel.toString());
-        System.out.println("this: "+ this.toString());
-        if (input != null) {
-            if (input.getClass().isInstance(newTunnel)) {
-                System.out.println("Found preexisting single input");
-                ci.setReturnValue(false);
-            } else {
-                System.out.println("Found preexisting single input of different type");
-                ci.setReturnValue(false);
-            }
-            } else {
-            System.out.println("setting single input");
-            //newTunnel.setOutput(false);
-            service.updateFreq(newTunnel, freq);
-            //onTunnelNetworkChange();
-            ci.setReturnValue(true);
-            }
+        setOutput(true);
     }
+
+    @Redirect(method = "importSettings", at = @At(ordinal = 0, value = "INVOKE", target = "setOutput"))
+    public void onSetOutput(P2PTunnelPart<?> thisPart, boolean isOutput) {}
 
     @Shadow
     abstract void setOutput(boolean output);
