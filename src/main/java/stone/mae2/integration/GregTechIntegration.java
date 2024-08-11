b4e12cf890216433002a79e365492d292b0eff7d
Raine <samstone9001@gmail.com>
2024-08-11 16:25:15 -0400

    Multi EU P2Ps Done!

--- src/main/java/stone/mae2/integration/GregTechIntegration.java
+++ src/main/java/stone/mae2/integration/GregTechIntegration.java
@@ -1,18 +1,29 @@
 package stone.mae2.integration;
 
+import com.gregtechceu.gtceu.api.capability.forge.GTCapability;
+
+import appeng.api.features.P2PTunnelAttunement;
 import appeng.api.parts.PartModels;
 import appeng.items.parts.PartItem;
 import appeng.items.parts.PartModelsHelper;
 import net.minecraft.Util;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.RegistryObject;
+import stone.mae2.api.features.MultiP2PTunnelAttunement;
 import stone.mae2.core.MAE2Items;
 import stone.mae2.parts.p2p.EUP2PTunnelPart;
 import stone.mae2.parts.p2p.PatternP2PTunnelPart;
+import stone.mae2.parts.p2p.multi.EUMultiP2PPart;
+import stone.mae2.parts.p2p.multi.PatternMultiP2PPart;
+import stone.mae2.util.TransHelper;
 
 public abstract class GregTechIntegration {
 	public static RegistryObject<PartItem<EUP2PTunnelPart>> EU_P2P_TUNNEL;
+	
+	public static RegistryObject<PartItem<EUMultiP2PPart>> EU_MULTI_P2P_TUNNEL;
 	
 	public static void init(IEventBus bus) {
 		EU_P2P_TUNNEL = Util.make(() -> {
@@ -22,5 +33,17 @@ public abstract class GregTechIntegration {
                     () -> new PartItem<>(new Item.Properties(),
                             EUP2PTunnelPart.class, EUP2PTunnelPart::new));
         });
+		
+		EU_MULTI_P2P_TUNNEL = Util.make(() -> {
+            PartModels.registerModels(
+                    PartModelsHelper.createModels(EUMultiP2PPart.class));
+            return MAE2Items.ITEMS.register("eu_multi_p2p_tunnel",
+                    () -> new PartItem<>(new Item.Properties(), EUMultiP2PPart.class, EUMultiP2PPart::new));
+        });
+		
+		bus.addListener((FMLCommonSetupEvent event) -> {
+			P2PTunnelAttunement.registerAttunementApi(EU_P2P_TUNNEL.get(), GTCapability.CAPABILITY_ENERGY_CONTAINER, Component.translatable(TransHelper.GUI.toKey("attunement", "euzz")));
+			MultiP2PTunnelAttunement.registerAttunementItem(EU_P2P_TUNNEL.get(), EU_MULTI_P2P_TUNNEL.get());
+		});
 	}
 }
---
+++ src/main/java/stone/mae2/parts/p2p/multi/EUMultiP2PPart.java
@@ -0,0 +1,265 @@
+package stone.mae2.parts.p2p.multi;
+
+import java.util.List;
+
+import com.gregtechceu.gtceu.api.capability.IEnergyContainer;
+import com.gregtechceu.gtceu.api.capability.forge.GTCapability;
+
+import appeng.api.parts.IPartItem;
+import appeng.api.parts.IPartModel;
+import appeng.items.parts.PartModels;
+import appeng.parts.p2p.P2PModels;
+import net.minecraft.core.Direction;
+import stone.mae2.MAE2;
+
+public class EUMultiP2PPart extends CapabilityMultiP2PPart<EUMultiP2PPart, IEnergyContainer> {
+
+	private static final P2PModels MODELS = new P2PModels(MAE2.toKey("part/p2p/multi_p2p_tunnel_eu"));
+	private static final IEnergyContainer EMPTY_HANDLER = new EmptyHandler();
+	
+    public EUMultiP2PPart(IPartItem<?> partItem) {
+        super(partItem, GTCapability.CAPABILITY_ENERGY_CONTAINER);
+        this.inputHandler = new InputHandler();
+        this.outputHandler = new OutputHandler();
+        this.emptyHandler = EMPTY_HANDLER;
+    }
+    
+    @PartModels
+    public static List<IPartModel> getModels() {
+        return MODELS.getModels();
+    }
+    
+    @Override
+    public IPartModel getStaticModels() {
+        return MODELS.getModel(this.isPowered(), this.isActive());
+    }
+
+    public class InputHandler implements IEnergyContainer {
+
+        @Override
+        public long acceptEnergyFromNetwork(Direction side, long voltage, long amperage) {
+        	// TODO tax the power somehow
+            int total = 0;
+
+            final int outputTunnels = EUMultiP2PPart.this.getOutputs().size();
+
+            if (outputTunnels == 0 | voltage == 0) {
+                return 0;
+            }
+
+            final long packetsPerOutput = voltage / outputTunnels;
+            long overflow = packetsPerOutput == 0 ? voltage : voltage % packetsPerOutput;
+
+            for (EUMultiP2PPart target : EUMultiP2PPart.this.getOutputs()) {
+                try (CapabilityGuard capabilityGuard = target.getAdjacentCapability()) {
+                    final IEnergyContainer output = capabilityGuard.get();
+                    final long toSend = packetsPerOutput + overflow;
+                    final long received = output.acceptEnergyFromNetwork(target.getSide().getOpposite(), toSend, amperage);
+
+                    overflow = toSend - received;
+                    total += received;
+                }
+            }
+
+            return total;
+        }
+
+        @Override
+        public boolean inputsEnergy(Direction side) {
+            return EUMultiP2PPart.this.getSide() == side;
+        }
+
+        @Override
+        public long changeEnergy(long differenceAmount) {
+            return 0;
+        }
+
+        @Override
+        public long getEnergyStored() {
+            long total = 0;
+
+            
+            for (EUMultiP2PPart part : EUMultiP2PPart.this.getOutputs()) {
+                try (CapabilityGuard guard = part.getAdjacentCapability()) {
+                    try {
+                        total = Math.addExact(total, guard.get().getEnergyStored());
+                    } catch (ArithmeticException e) {
+                        // combined output's storing more than a long's worth of power, return 0
+                        // instead, because otherwise it'll look like it'll look full, ie storing a long
+                        // of power, has a max capacity of a long -> full storage
+                        return 0;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        @Override
+        public long getEnergyCapacity() {
+            long total = 0;
+
+            
+            for (EUMultiP2PPart part : EUMultiP2PPart.this.getOutputs()) {
+                try (CapabilityGuard guard = part.getAdjacentCapability()) {
+                    try {
+                        total = Math.addExact(total, guard.get().getEnergyCapacity());
+                    } catch (ArithmeticException e) {
+                        // combined output's capacity is more than a long's worth of power, return max long
+                        // instead, because otherwise it'll look like it'll look full, ie storing a long
+                        // of power, has a max capacity of a long -> full storage
+                        return Long.MAX_VALUE;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        @Override
+        public long getInputAmperage() {
+            long total = 0;
+
+            
+            for (EUMultiP2PPart part : EUMultiP2PPart.this.getOutputs()) {
+                try (CapabilityGuard guard = part.getAdjacentCapability()) {
+                    try {
+                        total = Math.addExact(total, guard.get().getInputAmperage());
+                    } catch (ArithmeticException e) {
+                        // combined outputs want more than a long's worth of power, return max long
+                        return Long.MAX_VALUE;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        @Override
+        public long getInputVoltage() {
+            long maxVoltage = 0;
+
+            
+            for (EUMultiP2PPart part : EUMultiP2PPart.this.getOutputs()) {
+                try (CapabilityGuard guard = part.getAdjacentCapability()) {
+                    maxVoltage = Math.max(maxVoltage, guard.get().getInputVoltage());
+                }
+            }
+
+            return maxVoltage;
+        }
+
+    }
+    
+    public class OutputHandler implements IEnergyContainer {
+
+		@Override
+		public long acceptEnergyFromNetwork(Direction side, long voltage, long amperage) {
+			return 0;
+		}
+
+		@Override
+		public boolean inputsEnergy(Direction side) {
+			return false;
+		}
+
+		@Override
+		public long changeEnergy(long differenceAmount) {
+			return 0;
+		}
+
+		@Override
+		public long getEnergyStored() {
+long total = 0;
+
+            
+            for (EUMultiP2PPart part : EUMultiP2PPart.this.getInputs()) {
+                try (CapabilityGuard guard = part.getAdjacentCapability()) {
+                    try {
+                        total = Math.addExact(total, guard.get().getEnergyStored());
+                    } catch (ArithmeticException e) {
+                        // combined output's storing more than a long's worth of power, return 0
+                        // instead, because otherwise it'll look like it'll look full, ie storing a long
+                        // of power, has a max capacity of a long -> full storage
+                        return 0;
+                    }
+                }
+            }
+
+            return total;
+		}
+
+		@Override
+		public long getEnergyCapacity() {
+long total = 0;
+
+            
+            for (EUMultiP2PPart part : EUMultiP2PPart.this.getInputs()) {
+                try (CapabilityGuard guard = part.getAdjacentCapability()) {
+                    try {
+                        total = Math.addExact(total, guard.get().getEnergyCapacity());
+                    } catch (ArithmeticException e) {
+                        // combined output's capacity is more than a long's worth of power, return max long
+                        // instead, because otherwise it'll look like it'll look full, ie storing a long
+                        // of power, has a max capacity of a long -> full storage
+                        return Long.MAX_VALUE;
+                    }
+                }
+            }
+
+            return total;
+		}
+
+		@Override
+		public long getInputAmperage() {
+			return 0;
+		}
+
+		@Override
+		public long getInputVoltage() {
+			return 0;
+		}
+    	
+    }
+    
+    private static class EmptyHandler implements IEnergyContainer {
+
+		@Override
+		public long acceptEnergyFromNetwork(Direction side, long voltage, long amperage) {
+			return 0;
+		}
+
+		@Override
+		public boolean inputsEnergy(Direction side) {
+			return false;
+		}
+
+		@Override
+		public long changeEnergy(long differenceAmount) {
+			return 0;
+		}
+
+		@Override
+		public long getEnergyStored() {
+			return 0;
+		}
+
+		@Override
+		public long getEnergyCapacity() {
+			return 0;
+		}
+
+		@Override
+		public long getInputAmperage() {
+			return 0;
+		}
+
+		@Override
+		public long getInputVoltage() {
+			return 0;
+		}
+    	
+    }
+    
+}
+
