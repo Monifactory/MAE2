4c7106ac29a374120d3a6ebd20cef8e8dd726f2e
Raine Stone <samstone9001@gmail.com>
2024-10-23 14:44:03 -0400

    Switch over to our own Built-In model loader

--- src/main/java/stone/mae2/core/Proxy.java
+++ src/main/java/stone/mae2/core/Proxy.java
@@ -3,8 +3,6 @@ package stone.mae2.core;
 import appeng.api.integrations.igtooltip.PartTooltips;
 import appeng.api.networking.GridServices;
 import appeng.client.render.crafting.CraftingCubeModel;
-import appeng.core.AppEng;
-import appeng.hooks.BuiltInModelHooks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.network.chat.Component;
@@ -23,6 +21,7 @@ import stone.mae2.api.features.MultiP2PTunnelAttunement;
 import stone.mae2.block.crafting.DynamicCraftingUnitType;
 import stone.mae2.client.render.crafting.DynamicCraftingCubeModelProvider;
 import stone.mae2.core.datagen.MAE2RecipeProvider;
+import stone.mae2.hooks.BuiltInModelHooks;
 import stone.mae2.integration.GregTechIntegration;
 import stone.mae2.integration.MultiP2PStateDataProvider;
 import stone.mae2.me.service.MultiP2PService;
@@ -33,9 +32,8 @@ public interface Proxy {
         public void init(IEventBus bus) {
             MAE2Blocks.init(bus);
             MAE2Items.init(bus);
-            
-            if (ModList.get().isLoaded("gtceu"))
-            {
+
+            if (ModList.get().isLoaded("gtceu")) {
                 GregTechIntegration.init(bus);
             }
 
@@ -75,26 +73,27 @@ public interface Proxy {
                                 }
                             }).build());
 
-            // have to use AE2's id because their model loader checks for the id
             BuiltInModelHooks.addBuiltInModel(
-                    new ResourceLocation(AppEng.MOD_ID, "block/crafting/4x_accelerator_formed"),
+                    MAE2.toKey("block/crafting/4x_accelerator_formed"),
                     new CraftingCubeModel(
                             new DynamicCraftingCubeModelProvider(DynamicCraftingUnitType.ACCELERATOR_4x)));
 
             BuiltInModelHooks.addBuiltInModel(
-                    new ResourceLocation(AppEng.MOD_ID, "block/crafting/16x_accelerator_formed"),
+                    MAE2.toKey("block/crafting/16x_accelerator_formed"),
                     new CraftingCubeModel(
                             new DynamicCraftingCubeModelProvider(DynamicCraftingUnitType.ACCELERATOR_16x)));
 
             BuiltInModelHooks.addBuiltInModel(
-                    new ResourceLocation(AppEng.MOD_ID, "block/crafting/64x_accelerator_formed"),
+                    MAE2.toKey("block/crafting/64x_accelerator_formed"),
                     new CraftingCubeModel(
                             new DynamicCraftingCubeModelProvider(DynamicCraftingUnitType.ACCELERATOR_64x)));
 
-            BuiltInModelHooks.addBuiltInModel(
-                    new ResourceLocation(AppEng.MOD_ID, "block/crafting/256x_accelerator_formed"),
-                    new CraftingCubeModel(new DynamicCraftingCubeModelProvider(
-                            DynamicCraftingUnitType.ACCELERATOR_256x)));
+            BuiltInModelHooks
+                    .addBuiltInModel(
+                            MAE2.toKey("block/crafting/256x_accelerator_formed"),
+                            new CraftingCubeModel(
+                                    new DynamicCraftingCubeModelProvider(
+                                            DynamicCraftingUnitType.ACCELERATOR_256x)));
 
             PartTooltips.addBody(MultiP2PTunnelPart.class, new MultiP2PStateDataProvider());
         }
---
+++ src/main/java/stone/mae2/mixins/ModelBakeryMixin.java
@@ -0,0 +1,35 @@
+package stone.mae2.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.UnbakedModel;
+import net.minecraft.resources.ResourceLocation;
+
+import stone.mae2.hooks.BuiltInModelHooks;
+
+/**
+ * Replicates the part of the Fabric API for adding built-in models that we actually use.
+ *
+ * Copied from AE2
+ */
+@Mixin(ModelBakery.class)
+public class ModelBakeryMixin {
+    @Inject(at = @At("HEAD"), method = "loadModel", cancellable = true)
+    private void loadModelHook(ResourceLocation id, CallbackInfo ci) {
+        var model = BuiltInModelHooks.getBuiltInModel(id);
+
+        if (model != null) {
+            cacheAndQueueDependencies(id, model);
+            ci.cancel();
+        }
+    }
+
+    @Shadow
+    protected void cacheAndQueueDependencies(ResourceLocation id, UnbakedModel unbakedModel) {
+    }
+}
