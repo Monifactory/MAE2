af80713deb1c593cadde10b386b37e61081a2eb8
Raine Stone <samstone9001@gmail.com>
2024-11-08 11:07:22 -0500

    Optimize AoEPaste's iteration
    It used to iterate radius^3 times over an area of radius^2 wich wasted time. Still has a for loop that only "loops" once, but hopefully Java optimizes that out.

--- src/main/java/stone/mae2/item/faulty/AoEPaste.java
+++ src/main/java/stone/mae2/item/faulty/AoEPaste.java
@@ -5,6 +5,7 @@ import appeng.api.parts.IPart;
 import appeng.api.parts.IPartHost;
 import appeng.api.parts.SelectedPart;
 import appeng.api.util.AEColor;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -59,14 +60,18 @@ public class AoEPaste extends FaultyCardMode {
             SelectedPart selectedPart = partHost.selectPartWorld(context.getClickLocation());
             if (selectedPart.part != null && selectedPart.side != null && stack.getItem() instanceof IMemoryCard card) {
                 CompoundTag data = card.getData(stack);
+                // 0 if it's along the normal of the plane of AoE
+                // (the methods are accessing the normal variable)
                 int x = selectedPart.side.getStepX() != 0 ? 0 : 1;
                 int y = selectedPart.side.getStepY() != 0 ? 0 : 1;
                 int z = selectedPart.side.getStepZ() != 0 ? 0 : 1;
+                BlockPos clicked = context.getClickedPos().immutable();
                 // TODO optimize the iteration, maybe something that just iterates radius^2 times and converts the index to coords
-                for (int i = -radius; i <= radius; i++) {
-                    for (int j = -radius; j <= radius; j++) {
-                        for (int k = -radius; k <= radius; k++) {
-                            BlockEntity aoeBE = level.getBlockEntity(context.getClickedPos().offset(i * x, j * y, k * z));
+                for (int i = -radius * x; i <= radius * x; i++) {
+                    for (int j = -radius * y; j <= radius * y; j++) {
+                        for (int k = -radius * z; k <= radius * z; k++) {
+                            BlockEntity aoeBE = level.getBlockEntity(clicked.offset(i, j, k));
+                            MAE2.LOGGER.info("Iterated over Faulty Card!");
                             if (aoeBE instanceof IPartHost aoePartHost) {
                                 IPart part = aoePartHost.getPart(selectedPart.side);
                                 if (part != null) {
